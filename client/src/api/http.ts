c0538ddabbb7a791fc4c4bdb7c3a44290e327a68
aziz rajhi <rajhiaziz2@gmail.com>
2026-04-02 02:52:17 +0100

    http

--- client/src/api/http.ts
+++ client/src/api/http.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5001/api',
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://https://process-monitoring-app-3.onrender.com/api',
   timeout: 10000,
 });
 

dcbf545161083d50cc2a66b37b67bd94349aaa76
aziz rajhi <rajhiaziz2@gmail.com>
2026-04-02 02:18:07 +0100

    corr

--- client/src/components/process/ProcessHistoryDialog.tsx
+++ client/src/components/process/ProcessHistoryDialog.tsx
@@ -8,7 +8,6 @@ import Button from '@mui/material/Button';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Stack from '@mui/material/Stack';
